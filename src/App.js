e2dd9a1654c6afe9ac54e7a2881bf64bc3048f7d
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2022-02-09 14:58:34 +0900

    No Way Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Cctv from './components/Cctv';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/Cctv" element={<Cctv/>} />
+        <Route path="/" element={<Home/>} exact={true} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Contact" element={<Contact/>} />
+      </Routes>
+    </Router>
   );
 }
-
+// route로 감쌀 때 componet가 아니라 element
+// v6 쓰면서 조금 바뀐듯 ?
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React, { Component, useState, useEffect } from 'react';
+//import { BrowserRouter, Route } from 'react-router-dom';
+import WeatherInfo from './WeatherInfo';
+
+class Hello extends Component {
+	render() {
+		return (
+			<div>
+				<h1>im hello</h1>
+				<a
+					href="#!"
+					onClick={function (e) {
+						e.preventDefault();
+						this.props.getMessage();
+					}.bind(this)}>{this.props.message}</a>
+				<a
+					href="#!">{console.log("여긴 home")}</a>
+			</div>
+		);
+	}
+}
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: { test: "who am i" }
+		}
+	}
+	render() {
+		return (
+			<div className="Home">
+				<Hello
+					message={this.state.message.test}
+					getMessage={
+						function (e) {
+							//this.setState({ message: { test: "fuck" } });
+							console.log("받아옵니다");
+							fetch('http://localhost:3001')
+								.then(res => res.json())
+								.then(data => this.setState({ message: { test: data.test } }));
+						}.bind(this)
+					}></Hello>
+				<WeatherInfo></WeatherInfo>
+			</div >
+		);
+	}
+}
+
+export default Home;
