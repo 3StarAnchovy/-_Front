e6582a5fb5b51928c65fb3358237b8a3ebbf1cda
Chung Hyun Mok <hyunmok@Chungui-MacBookAir.local>
2022-02-19 19:52:49 +0900

    로그인 레이아웃

---
+++ src/components/User/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Form from "react-bootstrap/Form"; 
+import Button from "react-bootstrap/Button";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Login(){
+    return (
+        <div>
+            <Container className="panel" style={{marginTop: '30px', marginBottom: '30px', maxWidth: '300px'}}>
+                <Form>
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                        <Col>
+                            <Form.Control type="password" placeholder="UserID"/>
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                        <Col>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Col>
+                    </Form.Group>
+                    <br/>
+
+                    <div className="d-grid gap-1">
+                        <Button variant="primary" type="submit" size="lg">
+                            로그인
+                        </Button>
+                    </div>
+                </Form>
+            </Container>
+        </div>
+    );
+}
+
+export default Login
