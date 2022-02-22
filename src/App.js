b031d2d31f6510dbe337cbf658d4f52f025921a8
Chung Hyun Mok <hyunmok@Chungui-MacBookAir.local>
2022-02-22 22:13:24 +0900

    fix app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Login from './components/User/Login';
-import SignUp from './components/User/SignUp';
+import SignUp from './components/User/Signup';
 
 function App() {
   return (
